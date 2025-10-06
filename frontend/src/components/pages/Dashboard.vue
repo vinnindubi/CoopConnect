739a6a87659254bf7f55325dc37ea8ab15dbcd98
Vincent <128141904+vinnindubi@users.noreply.github.com>
2025-10-07 00:33:59 +0300

    updated card

--- frontend/src/components/pages/Dashboard.vue
+++ frontend/src/components/pages/Dashboard.vue
@@ -1,5 +1,31 @@
 <script setup lang="ts">
 </script>
 <template>
-    <h3>This is the landing page</h3>
+  <v-container> 
+    <v-row >
+        <v-col col>
+            <v-card class="mr-3 d-flex  justify-center">Student of the Week</v-card>
+        </v-col>
+        <v-col>
+            <v-card class="mr-3 d-flex  justify-center">Item two</v-card>
+         </v-col>
+         <v-col>
+            <v-card class="mr-3 d-flex  justify-center">Item three</v-card>
+        </v-col>
+    </v-row>
+    <v-row>
+        <v-col class="d-flex justify-center">Recent Activities Feed</v-col>
+    </v-row>
+    <v-container class="d-flex align-center justify-center">
+    <v-card :width="1000" :height="300"
+    elevation="5"
+    rounded="lg">
+    <v-card-item>
+        <v-card-title class="text-xl" >Card</v-card-title>
+        <v-divider></v-divider>
+    </v-card-item>
+    </v-card>
+</v-container>
+
+  </v-container>
 </template>
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'd358ffba2f05e5213cf55e3a2855a713944414b1',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'd358ffba2f05e5213cf55e3a2855a713944414b1',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
